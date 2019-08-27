d2f34ce3c434b34cd2dff3f378844d77352c9525
belloadewalea@gmail.com <belloadewalea@gmail.com>
2019-08-27 17:19:14 +0100

    video 53

--- ES6_tut/javascript.js
+++ ES6_tut/javascript.js
@@ -1,7 +1,7 @@
-function cars(brand){
-    if(!brand){
-        brand = "Ford"
-    }
+function cars(brand = "Ford1"){
+    // if(!brand){
+    //     brand = "Ford"
+    // }
     console.log(`My brand is ${brand}`)
 }
 
